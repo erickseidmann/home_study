4100a4feda8591d39127b97a7e7de177beb263d7
erickseidmann <erickseidmann@gmail.com>
2022-09-02 09:28:00 -0300

    encontrei o bug era uma letra minuscula errada

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -2,7 +2,7 @@
 class MobileNavbar{
     constructor(mobileMenu, navList, navLinks){
         this.mobileMenu = document.querySelector(mobileMenu);
-        this.navlist = document.querySelector(navList);
+        this.navList = document.querySelector(navList);
         this.navLinks = document.querySelector(navLinks);
         this.activeClass = "active";
 
